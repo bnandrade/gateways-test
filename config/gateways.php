e151e6af9c076ba8768f038dfa8aff0e9a51c43f
Bruno Andrade <bruno_weusa@hotmail.com>
2025-11-06 15:47:01 -0400

    feat: create gateways config file

---
+++ config/gateways.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'gateway1' => [
+        'url' => env('GATEWAY1_URL', 'http://gateway1:3001'),
+        'email' => env('GATEWAY1_EMAIL', 'dev@betalent.tech'),
+        'token' => env('GATEWAY1_TOKEN', 'FEC9BB078BF338F464F96B48089EB498'),
+    ],
+
+    'gateway2' => [
+        'url' => env('GATEWAY2_URL', 'http://gateway2:3002'),
+        'auth_token' => env('GATEWAY2_AUTH_TOKEN', 'tk_f2198cc671b5289fa856'),
+        'auth_secret' => env('GATEWAY2_AUTH_SECRET', '3d15e8ed6131446ea7e3456728b1211f'),
+    ],
+
+    'timeout' => env('GATEWAY_TIMEOUT', 30),
+    'retry_attempts' => env('GATEWAY_RETRY_ATTEMPTS', 2),
+];
